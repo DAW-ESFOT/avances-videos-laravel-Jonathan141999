6b2c112013bb030292fea5760bbd43c7ac62fb9b
Jona141999 <jona94alquinga@gmail.com>
2021-01-17 19:05:16 -0500

    Â#22 resources (Articles)

--- app/Http/Controllers/ArticleController.php
+++ app/Http/Controllers/ArticleController.php
@@ -4,15 +4,17 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Article;
+use App\Http\Resources\Article as ArticleResource;
+use App\Http\Resources\ArticleCollection;
 class ArticleController extends Controller
 {
     public function index()
     {
-        return Article::all();
+        return new ArticleCollection(Article::paginate(25));
     }
     public function show(Article $article)
     {
-        return $article;
+        return response()->json(new ArticleResource($article), 200);
     }
     public function store(Request $request)
     {
@@ -29,4 +31,5 @@ class ArticleController extends Controller
         $article->delete();
         return response()->json(null, 204);
     }
+
 }
