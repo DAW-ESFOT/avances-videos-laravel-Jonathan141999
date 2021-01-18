1db7ddb224ad154d22b1adc05190f8174b282fd3
Jona141999 <jona94alquinga@gmail.com>
2021-01-17 21:41:20 -0500

    #23 validacion de articles

--- app/Http/Controllers/ArticleController.php
+++ app/Http/Controllers/ArticleController.php
@@ -6,8 +6,18 @@ use Illuminate\Http\Request;
 use App\Article;
 use App\Http\Resources\Article as ArticleResource;
 use App\Http\Resources\ArticleCollection;
+use Illuminate\Support\Facades\Validator;
+
 class ArticleController extends Controller
 {
+    private static $rules = [
+        'title' => 'required|string|unique:articles|max:255',
+        'body' => 'required',
+    ];
+    private static $messages =[
+        'required' => 'El campo :attribute es obligatorio.',
+        'body.required' => 'El body no es valido.',
+    ];
     public function index()
     {
         return new ArticleCollection(Article::paginate(10));
@@ -18,6 +28,15 @@ class ArticleController extends Controller
     }
     public function store(Request $request)
     {
+        $request->validate(self::$rules,self::$messages);
+
+        //$validator = Validator::make($request->all(), [
+          //  'title' => 'required|string|unique:articles|max:255',
+            //'body' => 'required|string'
+        //]);
+        //if ($validator->fails()) {
+          //  return response()->json(['error' => 'data_validation_failed', "error_list"=>$validator->errors()], 400);
+        //}
         $article = Article::create($request->all());
         return response()->json($article, 201);
     }
