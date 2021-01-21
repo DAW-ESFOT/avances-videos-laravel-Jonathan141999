a3e30511a89084c4068c80aab831418a8fe84ced
Jona141999 <jona94alquinga@gmail.com>
2021-01-20 21:51:38 -0500

    #28 Implementacion manejo de imagenes mediante api

--- app/Http/Controllers/ArticleController.php
+++ app/Http/Controllers/ArticleController.php
@@ -6,6 +6,7 @@ use Illuminate\Http\Request;
 use App\Article;
 use App\Http\Resources\Article as ArticleResource;
 use App\Http\Resources\ArticleCollection;
+use Illuminate\Support\Facades\Storage;
 use Illuminate\Support\Facades\Validator;
 
 class ArticleController extends Controller
@@ -22,23 +23,28 @@ class ArticleController extends Controller
     {
         return response()->json(new ArticleResource($article), 200);
     }
+    public function image(Article $article)
+    {
+        return response()->download(public_path(Storage::url($article->image)), $article->title);
+    }
+
     public function store(Request $request)
     {
         $request->validate([
             'title' => 'required|string|unique:articles|max:255',
             'body' => 'required',
-            'category_id'=>'required|exists:categories,id'
+            'category_id'=>'required|exists:categories,id',
+            'image' => 'required|image|dimensions:min_width=200,min_height=200',
         ],self::$messages);
 
-        //$validator = Validator::make($request->all(), [
-          //  'title' => 'required|string|unique:articles|max:255',
-            //'body' => 'required|string'
-        //]);
-        //if ($validator->fails()) {
-          //  return response()->json(['error' => 'data_validation_failed', "error_list"=>$validator->errors()], 400);
-        //}
-        $article = Article::create($request->all());
-        return response()->json($article, 201);
+        //$article = Article::create($request->all());
+
+        $article = new Article($request->all());
+        $path = $request->image->store('public/articles');
+        $article->image = $path;
+        $article->save();
+
+        return response()->json(new ArticleResource($article), 201);
     }
     public function update(Request $request, Article $article)
     {
--- database/seeds/ArticlesTableSeeder.php
+++ database/seeds/ArticlesTableSeeder.php
@@ -19,6 +19,7 @@ class ArticlesTableSeeder extends Seeder
         // iteramos sobre cada uno y simulamos un inicio de
         // sesión con cada uno para crear artículos en su nombre
         $users = App\User::all();
+        $image_name = $faker->image('public/storage/articles', 400, 300, null, false);
         foreach ($users as $user) {
             // iniciamos sesión con este usuario
             JWTAuth::attempt(['email' => $user->email, 'password' => '123123']);
@@ -28,7 +29,8 @@ class ArticlesTableSeeder extends Seeder
                 Article::create([
                     'title' => $faker->sentence,
                     'body' => $faker->paragraph,
-                    'category_id' => $faker->numberBetween(1, 3)
+                    'category_id' => $faker->numberBetween(1, 3),
+                    'image' => 'articles/' . $image_name
                 ]);
             }
         }
