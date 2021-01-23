0520665f35bcf92f0df9bf1647cec32db4dce54c
Jona141999 <jona94alquinga@gmail.com>
2021-01-23 15:44:43 -0500

    #31 Relaciones Poli 1

--- app/User.php
+++ app/User.php
@@ -89,4 +89,8 @@ class User extends Authenticatable  implements JWTSubject
         }
         return false;
     }
+    public function userable()
+    {
+        return $this->morphTo();
+    }
 }
